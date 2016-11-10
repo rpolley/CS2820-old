59b407ec198b5b3256ad246442c85e6aceb9d411
jjreinhart <jonathan-reinhart@uiowa.edu>
2016-11-10 11:21:56 -0600

    Update Test.java
    Made test work

--- oo-warehouse/src/Test.java
+++ oo-warehouse/src/Test.java
@@ -1,9 +1,7 @@
-
 public class Test {
 	public static void main(String[] args) {
 		RobotScheduler test = new RobotScheduler();
 		Robot testy = new Robot(5, 5);
-		System.out.println(testy.batterylife);
 		int[] temp = new int[2];
 		temp[0]=testy.x;
 		temp[1]=testy.y;
@@ -12,6 +10,6 @@ public class Test {
 		temp[1]=3;
 		test.ShelvesLocs.put(0, temp);
 		test.RequestQueue.add(0);
-		test.MovingRobots();
+		test.MovingRobotstoShelf();
 	}
 }
