8eac19dda0e45902010b23aee70adaa6f96fe52b
james-vipond <james-vipond@uiowa.edu>
2016-11-14 20:21:00 -0600

    Preparation to implement FrameListner

--- oo-warehouse/src/Visualizer.java
+++ oo-warehouse/src/Visualizer.java
@@ -1,79 +1,67 @@
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
+import java.util.HashMap;
 
-public class Visualizer {
+public class Visualizer{
     /**
      * @wbp.parser.entryPoint
      */
-	private static final String floorFile = "res/Floor.txt";
-	private static BufferedReader floorReader = readFloor();
+	//private static final String floorFile = "res/Floor.txt";
+	//private static BufferedReader floorReader = readFloor();
+	private static Object[][] floor = Master.master.getFloor().layout;
+	private static HashMap<Robot, int[]> robots = Master.master.getRobotScheduler.RobotLocs;
+	private static HashMap<Integer,int[]> shelves = Master.master.getRobotScheduler.ShelvesLocs;
 	private static final ImageIcon beltIcon = new ImageIcon("res/Belt.png");
 	private static final ImageIcon highwayIcon = new ImageIcon("res/Highway.png");
 	private static final ImageIcon pickerIcon = new ImageIcon("res/Picker.png");
 	private static final ImageIcon packerIcon = new ImageIcon("res/Packer.png");
 	private static final ImageIcon chargerIcon = new ImageIcon("res/Charger.png");
 	private static final ImageIcon shelfZoneIcon = new ImageIcon("res/ShelfZone.png");
-
-
+	private JFrame window;
+	private JPanel panel; 
 	
-	private static BufferedReader readFloor(){
-		try {
-			return new BufferedReader(new FileReader(floorFile));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.out.println("Floor File Not Found");
-			return null;
-		}
+	public Visualizer(){
+		 JFrame frame = new JFrame("Warehouse Visualizer");
+	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        
+	     JPanel panel = new JPanel();
+	     frame.getContentPane().add(panel, BorderLayout.CENTER);
+	     panel.setLayout(new GridLayout(floor.length,floor[0].length));
+	     
+	     initialize();
+	     
+	     frame.pack();
+	        frame.setVisible(true);
 	}
 	
-    private static void initialize() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("Warehouse Visualizer");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private static void initialize() {       
         
-        JPanel panel = new JPanel();
-        frame.getContentPane().add(panel, BorderLayout.CENTER);
-        panel.setLayout(new GridLayout(10,10));
-        
-        for(int x = 0; x < 10; x++){
-        	String row;
-			try {
-				row = floorReader.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.out.println("Invalid Floor File");
-				break;
-			}
-        	for(int y = 0; y < 10; y++){
-        		String floorLocation = String.valueOf(row.charAt(y));
-        		switch(floorLocation){
-        			case "b":
+        for(int x = 0; x < floor.length; x++){
+        	for(int y = 0; y < floor[0].length; y++){
+        		Object here = floor[x][y];
+        		switch(here.getClass()){
+        			case (new Belt().getClass()):
         				panel.add(new JLabel(beltIcon));
         				continue;
-        			case "-":
+        			case (new Highway().getClass()):
         				panel.add(new JLabel(highwayIcon));
         				continue;
-        			case "c":
+        			case (new Charger().getClass()):
         				panel.add(new JLabel(chargerIcon));
         				continue;
-        			case "i":
+        			case (new Pick().getClass()):
         				panel.add(new JLabel(pickerIcon));
         				continue;
-        			case "a":
+        			case (new Pack().getClass()):
         				panel.add(new JLabel(packerIcon));
         				continue;
-        			case "s":
+        			case (new ShelfSpace.getClass()):
         				panel.add(new JLabel(shelfZoneIcon));
         				continue;
         		}
-        		//panel.add(new JLabel("[" + x + "," + y + "]"));
         	}
-        }
-        
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-
-    }
+        }     
+    }    
 }
+
